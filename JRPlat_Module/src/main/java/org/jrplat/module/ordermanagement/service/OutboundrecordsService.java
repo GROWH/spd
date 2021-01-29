d591746fcf27d1e6cc635c3e729d1936903d4f07
liurch <liuruncheng@swifthealth.cn>
2021-01-29 11:42:41 +0800

    接口需改

--- JRPlat_Module/src/main/java/org/jrplat/module/ordermanagement/service/OutboundrecordsService.java
+++ JRPlat_Module/src/main/java/org/jrplat/module/ordermanagement/service/OutboundrecordsService.java
@@ -1,9 +1,16 @@
 package org.jrplat.module.ordermanagement.service;
 
 
+import org.jrplat.module.ordermanagement.model.OrderInformation;
 import org.jrplat.module.ordermanagement.model.Outboundrecords;
+import org.jrplat.module.security.model.User;
+import org.jrplat.module.security.service.UserHolder;
+import org.jrplat.module.unitInfo.model.Unit;
 import org.jrplat.platform.service.SimpleService;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.Query;
+import java.util.List;
 
 /**
  * @Author:wangkang
@@ -11,4 +18,33 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OutboundrecordsService extends SimpleService<Outboundrecords> {
+
+
+
+    public List<Outboundrecords> queryOrderInformation(){
+        try {
+
+            //获取当前用户
+            User user = UserHolder.getCurrentLoginUser();
+            //获取当前登录用户的单位
+            Unit unit = user.getUnit();
+            String jpql = "select o from Outboundrecords o ";
+//            if(likeInfo != null && !likeInfo.equals("")){
+//                jpqlw += "and  w.workPBBarcode like '%"+likeInfo+"%'";
+//            }
+            Query queryw = getService().getEntityManager().createQuery(jpql, Outboundrecords.class);
+            if(unit != null){
+                jpql += "where o.unitNo.paperworkNo =:uNo";
+                queryw.setParameter("uNo", unit.getPaperworkNo());
+            }
+
+            List<Outboundrecords> orderManagementList = queryw.getResultList();
+            return orderManagementList;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
 }
